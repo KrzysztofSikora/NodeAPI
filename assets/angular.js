aa0c114ddc1aaff599e740e7306b1192842b528e
Cudako <robciu1993@gmail.com>
2016-05-31 11:45:15 +0200

    start page

--- assets/angular.js
+++ assets/angular.js
@@ -1,12 +1,12 @@
 /**
  * Created by Lenovo on 2016-05-31.
  */
-var app = angular.module("app",["ngRoute"],["ngResource"]);
+var app = angular.module("app",["ngRoute","ngResource"]);
 
 app.config(["$routeProvider"], function($routeProvider){
     "use strict";
    $routeProvider.when("/",{
-       templateUrl:"/partials/index.html"
+       templateUrl:"/partials/start.html"
    }).when('/check',{
        templateUrl:"/partials/result"
    }).otherwise({
