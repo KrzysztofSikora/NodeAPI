2882ca3bf5967121aa4bff77c2f84e712e57ad2f
Cudako <robciu1993@gmail.com>
2016-05-31 11:37:34 +0200

    ap

--- assets/angular.js
+++ assets/angular.js
@@ -1,11 +1,13 @@
 /**
  * Created by Lenovo on 2016-05-31.
  */
-var app = angular.module("Scripts",["ngRoute"],["ngResource"]);
+var app = angular.module("app",["ngRoute"],["ngResource"]);
 
 app.config(["$routeProvider"], function($routeProvider){
     "use strict";
-   $routeProvider.when('/check',{
+   $routeProvider.when("/",{
+       templateUrl:"/partials/index.html"
+   }).when('/check',{
        templateUrl:"/partials/result"
    }).otherwise({
        redirectTo:"/"
