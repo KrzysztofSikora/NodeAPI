078bc79baf3c0adbc2fa355b4deb1df34f383dba
Cudako <robciu1993@gmail.com>
2016-05-31 11:29:01 +0200

    ang sript

--- assets/angular.js
+++ assets/angular.js
@@ -1,3 +1,13 @@
 /**
  * Created by Lenovo on 2016-05-31.
  */
+var app = angular.module("Scripts",["ngRoute"],["ngResource"]);
+
+app.config(["$routeProvider"], function($routeProvider){
+    "use strict";
+   $routeProvider.when('/check',{
+       templateUrl:"/partials/result"
+   }).otherwise({
+       redirectTo:"/"
+   })
+});
