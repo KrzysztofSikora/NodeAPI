7a26bb7279dc6934de3155f7bd49f3af32e2cb12
Cudako <robciu1993@gmail.com>
2016-05-31 17:59:10 +0200

    KURWA JEBANY W PIZDE KOPANY NAWIAS

--- assets/angular.js
+++ assets/angular.js
@@ -1,15 +1,43 @@
 /**
  * Created by Lenovo on 2016-05-31.
  */
-var app = angular.module("app",["ngRoute","ngResource"]);
+var app = angular.module('app', ['ngRoute']);
 
-app.config(["$routeProvider"], function($routeProvider){
+app.config(["$routeProvider", function ($routeProvider) {
     "use strict";
-   $routeProvider.when("/",{
-       templateUrl:"/partials/start.html"
-   }).when('/check',{
-       templateUrl:"/partials/result"
-   }).otherwise({
-       redirectTo:"/"
-   })
-});
+    $routeProvider
+        .when("/", {
+            templateUrl: "/partials/start.html"
+        })
+        .when('/check', {
+            templateUrl: "/partials/result"
+        })
+        .otherwise({
+            redirectTo: "/"
+        });
+}]);
+//app.controller('IndexController',['$scope','$resource','$location',
+//    function($scope,$resource,$location){
+//        $scope.login = function(){
+//            var Users = $resource("/user/login");
+//            console.log("logujemy");
+//
+//            Users.save($scope.user,function(){
+//                console.log("logujemy");
+//            })
+//        }
+//    }]);
+//app.config(function ($routeProvider) {
+//    $routeProvider
+//        .when('/', {
+//            templateUrl: 'index1.html',
+//            controller: 'indexController1'
+//        })
+//        .when('/view/:id', {
+//            templateUrl: 'index2.html',
+//            controller: 'indexController2'
+//        })
+//        .otherwise({
+//            redirectTo: '/'
+//        });
+//});
