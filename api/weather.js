3505092418fb02d727e75b20b8009365cff79221
Cudako <robciu1993@gmail.com>
2016-06-01 20:35:44 +0200

    api works

--- api/weather.js
+++ api/weather.js
@@ -1,21 +1,24 @@
 /**
  * Created by Lenovo on 2016-06-01.
  */
-var express= require("express");
+var express = require("express");
 var router = express.Router();
+var http = require("http");
+var openWeather = require("openweathermap");
 
-// Adds
+// Add
+var WeatherUrl = "//api.openweathermap.org/data/2.5/weather?q=Chrzan%C3%B3w&appid=7626413626a82fc04fc9cd4ca52108a7";
 var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-router.get("/:id",function(req,res){
+router.get("/:id", function (req, res) {
     "use strict";
-    console.log("WYSYLAM DANE O POGODZIE");
-    res.json({
-        data:"someexampledata",
-        id:req.params.id
-    })
+    
+    openWeather.now({id:3101547 ,appid:"7626413626a82fc04fc9cd4ca52108a7"}, function(err, json){
+        res.json(json)
+    });
+
 });
 
 
